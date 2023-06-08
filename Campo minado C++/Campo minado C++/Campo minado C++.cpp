ac4753c483fd392e2f8ad3205df0b70dae37f6e3
Joao-Lucas-AP <jlucasap@hotmail.com>
2023-06-08 02:07:40 -0300

    Jogo finalizado!

--- Campo minado C++/Campo minado C++/Campo minado C++.cpp
+++ Campo minado C++/Campo minado C++/Campo minado C++.cpp
@@ -6,7 +6,7 @@
 using namespace sf;
 using namespace std;
 
-enum class GameState 
+enum class GameState
 {
 	MainMenu,
 	Playing,
@@ -23,15 +23,24 @@ int main()
 	int grid[12][12];
 	int sgrid[12][12];
 
+	//Carregar textura
 	Texture t;
 	t.loadFromFile("D:/CampoMinadoCPP/Campo minado C++/Campo minado C++/Images/tiles.jpg");
+	
 	Sprite s(t);
+	
+	//Carregar fonte
 	Font font;
 	font.loadFromFile("D:/CampoMinadoCPP/Campo minado C++/Campo minado C++/Images/PressStart2P-Regular.ttf");
 	
-	//Elementos do menu
+	bool playerWon = false;
+
+	int cellsRevealed = 0;
+
+	//Menu principal iniciado
 	GameState gameState = GameState::MainMenu;
 
+	//Elementos do menu
 	RectangleShape playButton(Vector2f(200, 80));
 	playButton.setFillColor(Color(100, 100, 100));
 	playButton.setPosition(app.getSize().x / 2 - playButton.getSize().x / 2, app.getSize().y / 2 - playButton.getSize().y / 2);
@@ -39,6 +48,16 @@ int main()
 	Text playButtonText("Play", font, 32);
 	playButtonText.setPosition(playButton.getPosition().x + playButton.getSize().x / 2 - playButtonText.getLocalBounds().width / 2,
 	playButton.getPosition().y + playButton.getSize().y / 2 - playButtonText.getLocalBounds().height / 2);
+
+	//Elementos da tela de game over
+	RectangleShape playAgainButton(Vector2f(200, 80));
+	playAgainButton.setFillColor(Color(100, 100, 100));
+	playAgainButton.setPosition(app.getSize().x / 2 - playAgainButton.getSize().x / 2, app.getSize().y / 2 - playAgainButton.getSize().y / 2);
+
+	Text playAgainButtonText("Play", font, 32);
+	playAgainButtonText.setPosition(playAgainButton.getPosition().x + playAgainButton.getSize().x / 2 - playAgainButtonText.getLocalBounds().width / 2,
+	playAgainButton.getPosition().y + playAgainButton.getSize().y / 2 - playAgainButtonText.getLocalBounds().height / 2);
+
 
 	while (app.isOpen())
 	{
@@ -59,10 +78,10 @@ int main()
 				{
 					if (playButton.getGlobalBounds().contains(pos.x, pos.y))
 					{
-						// Start game
+						//Iniciar o game
 						gameState = GameState::Playing;
 
-						// Initialize game grid and sgrid
+						//Inicializar grid e sgrid
 						for (int i = 1; i <= 10; i++)
 						{
 							for (int j = 1; j <= 10; j++)
@@ -80,22 +99,22 @@ int main()
 							}
 						}
 
-						for (int i = 1; i <= 10; i++) 
+						for (int i = 1; i <= 10; i++)
 						{
-							for (int j = 1; j <= 10; j++) 
+							for (int j = 1; j <= 10; j++)
 							{
 								int n = 0;
 
-								if (grid[i][j] == 9) 
+								if (grid[i][j] == 9)
 								{
 									continue;
 								}
 
-								for (int dx = -1; dx <= 1; dx++) 
+								for (int dx = -1; dx <= 1; dx++)
 								{
-									for (int dy = -1; dy <= 1; dy++) 
+									for (int dy = -1; dy <= 1; dy++)
 									{
-										if (grid[i + dx][j + dy] == 9) 
+										if (grid[i + dx][j + dy] == 9)
 										{
 											n++;
 										}
@@ -106,7 +125,6 @@ int main()
 							}
 						}
 
-						// Calculate numbers
 						for (int i = 1; i <= 10; i++)
 						{
 							for (int j = 1; j <= 10; j++)
@@ -165,19 +183,19 @@ int main()
 				}				
 			}
 			else if (gameState == GameState::Playing)
-			{
+			{ 
 				if (e.type == Event::MouseButtonPressed)
 				{
 					if (e.key.code == Mouse::Left)
 					{
 						if (grid[x][y] == 9)
 						{
-							// Game over logic
+							//Game over
 							gameState = GameState::GameOver;
 						}
 						else if (grid[x][y] == 0 && sgrid[x][y] == 10)
 						{
-							// Reveal empty cells and neighboring cells
+							//Revela células vazias e vizinhas
 							sgrid[x][y] = grid[x][y];
 
 							for (int i = x - 1; i <= x + 1; i++)
@@ -191,9 +209,36 @@ int main()
 								}
 							}
 						}
+						bool allCellsRevealed = true;
+
+						for (int i = 1; i <= 10; i++)
+						{
+							for (int j = 1; j <= 10; j++)
+							{
+								if (grid[i][j] != 9 && sgrid[i][j] == 10)
+								{
+									allCellsRevealed = false;
+									break;
+								}
+							}
+
+							if (!allCellsRevealed)
+							{
+								break;
+							}
+						}
+
+						if (allCellsRevealed)
+						{
+							//Vitória!
+							playerWon = true;
+							gameState = GameState::GameOver;
+						}
+
+
 						else if (sgrid[x][y] == 10)
 						{
-							// Reveal the cell
+							//Revela a célula
 							sgrid[x][y] = grid[x][y];
 						}
 					}
@@ -201,32 +246,145 @@ int main()
 					{
 						if (sgrid[x][y] == 10)
 						{
-							// Mark the cell as flagged
+							//Marca uma bandeira
 							sgrid[x][y] = 11;
 						}
 						else if (sgrid[x][y] == 11)
 						{
-							// Remove the flag from the cell
+							//Remove a bandeira
 							sgrid[x][y] = 10;
+						}
+					}
+				}
+			}
+
+			else if (gameState == GameState::GameOver)
+			{
+				if (e.type == Event::MouseButtonPressed && e.key.code == Mouse::Left)
+				{
+					if (playAgainButton.getGlobalBounds().contains(pos.x, pos.y))
+					{
+						//Iniciar o game
+						gameState = GameState::Playing;
+
+						//Inicializar grid e sgrid
+						for (int i = 1; i <= 10; i++)
+						{
+							for (int j = 1; j <= 10; j++)
+							{
+								sgrid[i][j] = 10;
+
+								if (rand() % 5 == 0)
+								{
+									grid[i][j] = 9;
+								}
+								else
+								{
+									grid[i][j] = 0;
+								}
+							}
+						}
+
+						for (int i = 1; i <= 10; i++)
+						{
+							for (int j = 1; j <= 10; j++)
+							{
+								int n = 0;
+
+								if (grid[i][j] == 9)
+								{
+									continue;
+								}
+
+								for (int dx = -1; dx <= 1; dx++)
+								{
+									for (int dy = -1; dy <= 1; dy++)
+									{
+										if (grid[i + dx][j + dy] == 9)
+										{
+											n++;
+										}
+									}
+								}
+
+								grid[i][j] = n;
+							}
+						}
+
+						for (int i = 1; i <= 10; i++)
+						{
+							for (int j = 1; j <= 10; j++)
+							{
+								int n = 0;
+
+								if (grid[i][j] == 9)
+								{
+									continue;
+								}
+
+								if (grid[i + 1][j] == 9)
+								{
+									n++;
+								}
+
+								if (grid[i][j + 1] == 9)
+								{
+									n++;
+								}
+
+								if (grid[i - 1][j] == 9)
+								{
+									n++;
+								}
+
+								if (grid[i][j - 1] == 9)
+								{
+									n++;
+								}
+
+								if (grid[i + 1][j + 1] == 9)
+								{
+									n++;
+								}
+
+								if (grid[i - 1][j - 1] == 9)
+								{
+									n++;
+								}
+
+								if (grid[i - 1][j + 1] == 9)
+								{
+									n++;
+								}
+
+								if (grid[i + 1][j - 1] == 9)
+								{
+									n++;
+								}
+
+								grid[i][j] = n;
+							}
 						}
 					}
 				}
 			}
 		}
 
+		//Desenho do game
 		app.clear(Color::White);
+
 		for (int i = 1; i <= 10; i++)
 		{
 			for (int j = 1; j <= 10; j++)
 			{
 				if (sgrid[i][j] == 11)
 				{
-					// Draw the flag sprite
+					//Desenha o sprite da bandeira
 					s.setTextureRect(IntRect(11 * w, 0, w, w));
 				}
 				else
 				{
-					// Draw the cell sprite based on its value
+					//Desenha o sprite da célula baseada no valor
 					s.setTextureRect(IntRect(sgrid[i][j] * w, 0, w, w));
 				}
 
@@ -234,6 +392,7 @@ int main()
 				app.draw(s);
 			}
 		}
+		//Desenho do menu principal
 		if (gameState == GameState::MainMenu)
 		{
 			RectangleShape backgroundRect(Vector2f(app.getSize().x, app.getSize().y));
@@ -249,7 +408,7 @@ int main()
 		}
 		else if (gameState == GameState::Playing)
 		{
-			// Draw the game grid
+			//Desenho do grid de gameplay
 			for (int i = 1; i <= 10; i++)
 			{
 				for (int j = 1; j <= 10; j++)
@@ -262,13 +421,52 @@ int main()
 		}
 		else if (gameState == GameState::GameOver)
 		{
-			// Draw the game over screen
-			RectangleShape gameOverBackground(Vector2f(400, 400));
-			gameOverBackground.setFillColor(Color(220, 220, 220));
+			//Desenho do game over
+			if (playerWon)
+			{
+				RectangleShape gObackgroundRect(Vector2f(app.getSize().x, app.getSize().y));
+				gObackgroundRect.setFillColor(Color::Green);
+				app.draw(gObackgroundRect);
 
-			app.draw(gameOverBackground);
+				RectangleShape playAgainButton(Vector2f(320, 80));
+				playAgainButton.setFillColor(Color::Black);
+				playAgainButton.setPosition(app.getSize().x / 2 - playAgainButton.getSize().x / 2, app.getSize().y / 2 - playAgainButton.getSize().y / 2);
+
+				Text gameOverTitleText("Victory", font, 32);
+				gameOverTitleText.setPosition(app.getSize().x / 2 - gameOverTitleText.getLocalBounds().width / 2, 100);
+
+				app.draw(gameOverTitleText);
+
+				Text playAgainButtonText("Play again", font, 30);
+				playAgainButtonText.setPosition(playAgainButton.getPosition().x + playAgainButton.getSize().x / 2 - playAgainButtonText.getLocalBounds().width / 2,
+					playAgainButton.getPosition().y + playAgainButton.getSize().y / 2 - playAgainButtonText.getLocalBounds().height / 2);
+
+				app.draw(playAgainButton);
+				app.draw(playAgainButtonText);
+			}
+			else 
+			{
+				RectangleShape gObackgroundRect(Vector2f(app.getSize().x, app.getSize().y));
+			    gObackgroundRect.setFillColor(Color::Red);
+			    app.draw(gObackgroundRect);
+
+			    RectangleShape playAgainButton(Vector2f(320, 80));
+			    playAgainButton.setFillColor(Color::Black);
+			    playAgainButton.setPosition(app.getSize().x / 2 - playAgainButton.getSize().x / 2, app.getSize().y / 2 - playAgainButton.getSize().y / 2);
+
+				Text gameOverTitleText("Game over", font, 32);
+				gameOverTitleText.setPosition(app.getSize().x / 2 - gameOverTitleText.getLocalBounds().width / 2, 100);
+
+				app.draw(gameOverTitleText);
+
+				Text playAgainButtonText("Play again", font, 30);
+				playAgainButtonText.setPosition(playAgainButton.getPosition().x + playAgainButton.getSize().x / 2 - playAgainButtonText.getLocalBounds().width / 2,
+				playAgainButton.getPosition().y + playAgainButton.getSize().y / 2 - playAgainButtonText.getLocalBounds().height / 2);
+
+				app.draw(playAgainButton);
+				app.draw(playAgainButtonText);
+			}		
 		}
-
 		app.display();
 	}
 }
